046c43474031a2ca621c9c2ce807d2c106e96245
Vu Hoang <vhoang@br005.pvt.bridges.psc.edu>
2017-06-27 19:17:52 -0400

    smaller dims for train.sh

--- train.sh
+++ train.sh
@@ -11,9 +11,9 @@ mkdir models
 mkdir models/multi30k-$1-$2/
  
 # Training
-rm -rf models/multi30k-$1-$2/baseline #remove the existing one if required!
+#rm -rf models/multi30k-$1-$2/baseline #remove the existing one if required!
 
 CUR_DIR=`pwd`
 
-PYTHONPATH=$CUR_DIR/sockeye python sockeye/sockeye/train.py --source data/multi30k/train.$1.atok --target data/multi30k/train.$2.atok --validation-source data/multi30k/val.$1.atok --validation-target data/multi30k/val.$2.atok --word-min-count 2 --rnn-num-layers 1 --rnn-cell-type lstm --rnn-num-hidden 128 --num-embed-source 128 --num-embed-target 128 --attention-type mlp --attention-num-hidden 128 --batch-size 64 --normalize-loss --dropout 0.1 --initial-learning-rate 0.001 --use-cpu --output models/multi30k-$1-$2/baseline
+PYTHONPATH=$CUR_DIR/sockeye python sockeye/sockeye/train.py --source data/multi30k/train.$1.atok --target data/multi30k/train.$2.atok --validation-source data/multi30k/val.$1.atok --validation-target data/multi30k/val.$2.atok --word-min-count 3 --rnn-num-layers 1 --rnn-cell-type gru --rnn-num-hidden 64 --num-embed-source 64 --num-embed-target 64 --attention-type mlp --attention-num-hidden 64 --batch-size 64 --normalize-loss --dropout 0.1 --optimizer adam --initial-learning-rate 0.001 --use-cpu --output models/multi30k-$1-$2/baseline
 
