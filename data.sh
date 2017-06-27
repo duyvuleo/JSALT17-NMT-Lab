3aa470b43b6447027cc6396d9c730cfc01ca32d3
Vu Hoang <vhoang@br005.pvt.bridges.psc.edu>
2017-06-27 10:59:50 -0400

    fixed paths

--- data.sh
+++ data.sh
@@ -1,8 +1,8 @@
 echo "Downloading the data..."
 echo "Targeting the Multi30k dataset..."
-mkdir -p ../data
-mkdir -p ../data/multi30k
-wget http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/training.tar.gz &&  tar -xf training.tar.gz -C ../data/multi30k && rm training.tar.gz
-wget http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/validation.tar.gz && tar -xf validation.tar.gz -C ../data/multi30k && rm validation.tar.gz
-wget https://staff.fnwi.uva.nl/d.elliott/wmt16/mmt16_task1_test.tgz && tar -xf mmt16_task1_test.tgz -C ../data/multi30k && rm mmt16_task1_test.tgz
+mkdir -p data
+mkdir -p data/multi30k
+wget http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/training.tar.gz &&  tar -xf training.tar.gz -C data/multi30k && rm training.tar.gz
+wget http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/validation.tar.gz && tar -xf validation.tar.gz -C data/multi30k && rm validation.tar.gz
+wget https://staff.fnwi.uva.nl/d.elliott/wmt16/mmt16_task1_test.tgz && tar -xf mmt16_task1_test.tgz -C data/multi30k && rm mmt16_task1_test.tgz
 
