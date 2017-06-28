1bb893555a35c40e83e73c08173c41a96454ea40
beckdaniel <daniel.e.beck@gmail.com>
2017-06-28 11:15:33 -0400

    added toy data generation

--- data.sh
+++ data.sh
@@ -6,3 +6,7 @@ wget http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/training.tar.gz &&  tar -xf
 wget http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/validation.tar.gz && tar -xf validation.tar.gz -C data/multi30k && rm validation.tar.gz
 wget https://staff.fnwi.uva.nl/d.elliott/wmt16/mmt16_task1_test.tgz && tar -xf mmt16_task1_test.tgz -C data/multi30k && rm mmt16_task1_test.tgz
 
+echo "Generating toy training data..."
+head -3000 data/multi30k/train.en > data/multi30k/train-toy.en
+head -3000 data/multi30k/train.de > data/multi30k/train-toy.de
+
