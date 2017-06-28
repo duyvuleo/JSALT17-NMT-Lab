4ac5ed35e8a583bbb8919e9591c137a13cc219fb
Gaurav Kumar <gaurav.bison@gmail.com>
2017-06-28 13:28:11 -0400

    env stuff for psc

--- ENV.sh
+++ ENV.sh
@@ -1,6 +1,7 @@
 # The tokenizer
-#export TOKENIZER=/pylon2/ci560op/fosterg/tools/mosesdecoder/scripts/tokenizer/tokenizer.perl
-export TOKENIZER=$MOSES/scripts/tokenizer/tokenizer.perl
+export TOKENIZER=/pylon2/ci560op/fosterg/tools/mosesdecoder/scripts/tokenizer/tokenizer.perl
+#export TOKENIZER=$MOSES/scripts/tokenizer/tokenizer.perl
 
-#export PYTHONPATH=$YOUR_SOCKEYE_INSTALL:$PYTHONPATH
-export SOCKEYE=/home/ws15gkumar/work/code/sockeye
+#export SOCKEYE=/home/ws15gkumar/work/code/sockeye
+export SOCKEYE=
+
