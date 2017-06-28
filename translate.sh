001150ff44de803fa7e8b03c27638a8003369eee
Vu Hoang <vhoang@br006.pvt.bridges.psc.edu>
2017-06-28 12:04:04 -0400

    fix hard-coded of translate.sh

--- translate.sh
+++ translate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash"
 echo "Translating/Decoding the input..."
 
-if test "$#" -ne 2; then
+if test "$#" -ne 3; then
         echo "Illegal number of parameters"
-        echo "sh translate.sh <inpiut> <output>"
+        echo "sh translate.sh <model> <input> <output>"
         exit 0
 fi
 
 CUR_DIR=`pwd`
-PYTHONPATH=$CUR_DIR/sockeye python sockeye/sockeye/translate.py --models models/multi30k-en-de/baseline --beam-size 5 --use-cpu < $1 > $2
+PYTHONPATH=$CUR_DIR/sockeye python sockeye/sockeye/translate.py --models $1 --beam-size 5 --use-cpu < $2 > $3
 
 
